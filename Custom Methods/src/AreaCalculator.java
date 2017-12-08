5bb1e0581dbce2c1d2bb247a3bde0d5f2d3cc892
tahana8108 <alextahini1@gmail.com>
2017-12-08 11:09:51 -0800

    Did part of Assignment 8

---
+++ Custom Methods/src/AreaCalculator.java
@@ -0,0 +1,89 @@
+import java.util.Scanner;
+
+public class AreaCalculator {
+	
+	public static void main(String[] args){
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.println("Shape Area Calculator (c) 2017 Alex Tahan ECS");
+		System.out.println("\n.............................................");
+		
+		System.out.println("\n1) Triangle");
+		System.out.println("2) Rectangle");
+		System.out.println("3) Square");
+		System.out.println("4) Circle");
+		System.out.println("5) Quit");
+		System.out.print("Which Shape: ");
+		int res = keyboard.nextInt();
+		System.out.print("\n");
+		
+		if (res == 1){
+			double area = triangle();
+			System.out.println("The area is "+area);
+		}
+		else if (res == 2){
+			double area = rectangle();
+			System.out.println("The area is "+area);
+		}
+		else if (res == 3){
+			double area = square();
+			System.out.println("The area is "+area);
+		}
+		else if (res == 4){
+			double area = circle();
+			System.out.println("The area is "+area);
+		}
+		
+		System.out.println("\nGoodbye!!!");
+		keyboard.close();
+		
+	}
+	public static double triangle(){
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.print("Base: ");
+		int b = keyboard.nextInt();
+		System.out.print("Height: ");
+		int h = keyboard.nextInt();
+		
+		double a = 0.5*b*h;
+		keyboard.close();
+		return a;
+	}
+	public static double rectangle(){
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.print("Length: ");
+		int l = keyboard.nextInt();
+		System.out.print("Width: ");
+		int w = keyboard.nextInt();
+		
+		double a = l*w;
+		keyboard.close();
+		return a;
+	}
+	public static double square(){
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.print("Side: ");
+		int s = keyboard.nextInt();
+		
+		double a = s*s;
+		keyboard.close();
+		return a;
+		
+		
+	}
+	public static double circle(){
+		Scanner keyboard = new Scanner(System.in);
+		
+		System.out.print("Radius: ");
+		int r = keyboard.nextInt();
+		
+		double a = Math.PI*r*r;
+		keyboard.close();
+		return a;
+		
+	}
+
+}
